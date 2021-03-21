1f32c8d2c26991445554e0a3d6341543aee904c8
Tailine Chagas <tailinechagas@gmail.com>
2021-03-21 10:04:36 -0300

    feat: pwa config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,6 @@
 module.exports = {
   "stories": ["../src/components/**/stories.tsx"],
   "addons": [
-    "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-knobs"
   ]
 }
--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,19 +1,13 @@
-import { withKnobs, text } from '@storybook/addon-knobs'
 import { Story, Meta } from '@storybook/react'
 import Main from '.'
 
 export default {
   title: 'Main',
-  component: Main,
-  decorators: [withKnobs]
+  component: Main
 } as Meta
 
-export const Basic: Story = () => (
-  <Main
-    title={text('Title', 'React Avançado')}
-    description={text(
-      'Description',
-      'Typescript, ReactJS, NextJS e Styled Components'
-    )}
-  />
-)
+export const Basic: Story = (args) => <Main {...args} />
+Basic.args = {
+  title: 'React Avançado',
+  description: 'Typescript, ReactJS, NextJS e Styled Components'
+}
